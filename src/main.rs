2e5aa4156895c37ba5206d53d703665e907124e5
Knarkzel <knarkzel@gmail.com>
2020-12-18 23:11:29 +0100

    works very nicely now, added tests passing path

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::io::Read;
-use std::matches;
+use std::{io::Read, matches, num::Wrapping};
 
 struct Brainfuck {
-    memory: [u8; 2usize.pow(16)],
+    memory: [Wrapping<u8>; 2usize.pow(16)],
     instructions: Vec<char>,
     data_pointer: usize,
     instruction_pointer: usize,
@@ -16,30 +15,25 @@ impl Brainfuck {
             .filter(|c| matches!(c, '>' | '<' | '+' | '-' | ',' | '.' | '[' | ']'))
             .collect();
         Brainfuck {
-            memory: [0; 2usize.pow(16)],
+            memory: [Wrapping(0); 2usize.pow(16)],
             instructions,
             data_pointer: 0,
             instruction_pointer: 0,
         }
     }
-    fn run(&mut self) {
-        while self.instruction_pointer < self.instructions.len() {
-            self.step();
-        }
-    }
     fn step(&mut self) {
         let command = self.instructions[self.instruction_pointer];
-        let value = self.memory[self.data_pointer];
+        let value = self.memory[self.data_pointer].0;
         match command {
             '>' => self.data_pointer += 1,
             '<' => self.data_pointer -= 1,
-            '+' => self.memory[self.data_pointer] += 1,
-            '-' => self.memory[self.data_pointer] -= 1,
+            '+' => self.memory[self.data_pointer] += Wrapping(1),
+            '-' => self.memory[self.data_pointer] -= Wrapping(1),
             '.' => print!("{}", value as char),
             ',' => {
                 let input = std::io::stdin().bytes().next();
                 if let Some(Ok(input)) = input {
-                    self.memory[self.data_pointer] = input;
+                    self.memory[self.data_pointer] = Wrapping(input);
                 }
             }
             '[' => {
@@ -51,7 +45,7 @@ impl Brainfuck {
             }
             ']' => {
                 if value != 0 {
-                    let slice = &self.instructions[..self.instruction_pointer];
+                    let slice = &self.instructions[..=self.instruction_pointer];
                     let matching_bracket = find_matching((']', '['), &mut slice.iter().rev());
                     self.instruction_pointer -= matching_bracket.unwrap();
                 }
@@ -60,26 +54,39 @@ impl Brainfuck {
         }
         self.instruction_pointer += 1;
     }
+    fn run(&mut self) {
+        while self.instruction_pointer < self.instructions.len() {
+            self.step();
+        }
+    }
 }
 
-// returns offset of matching bracket's index from current index using slices
-fn find_matching<'a>(brackets: (char, char), iter: &mut impl Iterator<Item = &'a char>) -> Option<usize> {
-    let mut counter = 1;
+fn find_matching<'a>(
+    brackets: (char, char),
+    iter: &mut impl Iterator<Item = &'a char>,
+) -> Option<usize> {
+    let mut counter = 0;
+    let (start, end) = brackets;
     for (i, item) in iter.enumerate() {
-        if item == &brackets.0 {
+        if item == &start {
             counter += 1;
         }
-        if item == &brackets.1 {
+        if item == &end {
             counter -= 1;
         }
         if counter == 0 {
-            return Some(i + 1);
+            return Some(i);
         }
     }
     None
 }
 
 fn main() {
-    let mut brainfuck = Brainfuck::new("./input");
-    brainfuck.run();
+    let path = std::env::args().skip(1).next();
+    if let Some(path) = path {
+        let mut brainfuck = Brainfuck::new(path);
+        brainfuck.run();
+    } else {
+        println!("Missing path to source file.");
+    }
 }
